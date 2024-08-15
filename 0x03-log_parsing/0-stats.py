ec6c8cbb5ec3ccb58332117ef6fc1b73467bfd2b
Itohan <momoduitohan5@gmail.com>
2024-08-15 22:48:57 +0100

    Write a script that reads stdin line by line and computes metrics

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""""""
+import sys
+import random
+
+i = 0
+status_code = []
+total_size = 0
+
+for line in sys.stdin:
+    line = line.strip()
+    string_line = str(line)
+    split_it = int(string_line.split(" ")[-2])
+    file_size = int(string_line.split(" ")[-1])
+    total_size += file_size
+    if split_it not in status_code:
+        status_code.append(split_it)
+        sort_it = sorted(status_code)
+    if i % 10 == 0 or KeyboardInterrupt:
+        print(f"File size: {total_size}")
+    for i in sort_it:
+        d_random = random.randint(1, 6)
+        if i != 500:
+         print(f"{i}: {d_random}")
+        if i == 500:
+            print(f"{i}: {d_random}")
+            print(f"File size: {total_size}")
+    
+i += 1
