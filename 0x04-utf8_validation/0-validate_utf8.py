4cb7f3bec7759771b8cb8235afbacd31db206a3b
Itohan <momoduitohan5@gmail.com>
2024-08-26 10:00:08 +0100

    Check for UTF-8 validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+    Write a method that determines
+    if a given data set represents
+    a valid UTF-8 encoding.
+"""
+
+
+def validUTF8(data):
+    """True if data is a valid UTF-8 encoding, else return False"""
+
+    n = 0
+    for v in data:
+        if n > 0:
+            if v >> 6 == 0b10:
+                return True
+            else:
+                return False
+        elif n == 0:
+            if v >> 7 == 0:
+                continue
+            if v >> 5 == 0b110:
+                n = 1
+            if v >> 4 == 0b1110:
+                n == 2
+            if v >> 3 == 0b11110:
+                n = 3
+        else:
+            return False
+        n += 1
+
+    if n != 0:
+        return False
+    else:
+        return True
